035f530cfe77af80c81efe130bcfd13f871efd22
arydianprtma <arydianprtma@gmail.com>
2026-02-01 13:58:55 +0700

    fix: update page title and add favicon using Logo Riyad.png

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,10 @@
 
     <title inertia>{{ config('app.name', 'Pondok Pesantren Riyadussalikin') }}</title>
 
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="/Logo Riyad.png">
+    <link rel="apple-touch-icon" href="/Logo Riyad.png">
+
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700|amiri:400,700" rel="stylesheet" />
